6b838734d74d50058cd9715010bf0ba388c4e8c5
Crowdbotics <team@crowdbotics.com>
2020-10-22 07:15:23 +0000

    Update models

--- backend/chartly_21820/wsgi.py
+++ backend/chartly_21820/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chartly_21820.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chartly_21820.settings")
 
 application = get_wsgi_application()
